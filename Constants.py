c4487f13a833c0b2385b63c11d1a8d5325e1f973
jhepker6536 <jhepker@gmail.com>
2015-03-18 12:57:55 -0400

    lost

--- Constants.py
+++ Constants.py
@@ -7,6 +7,7 @@ WHITE    = ( 255, 255, 255)
 GREEN    = (   0, 255,   0)
 RED      = ( 255,   0,   0)
 BLUE     = (  16,  81, 148)
+GREENS = (181,230,29)
 PURPLE   = ( 163,  73, 164)
 YELLOW   = (255, 242, 0)
 GREEN    = (34, 177, 76)
@@ -16,9 +17,8 @@ CLOUD_100 = (104 ,0, 158, 49)
 GRASS_200 = (269, 0, 200, 40)
 GRASS_300 = (0, 55, 300, 40)
 GRASS_400 = (41, 113, 400, 40)
-
+game_over = False
 level = 1 
-snake_limit_one = 400
-snake_limit_two = 766
+
 width = 1366
 hight = 768
